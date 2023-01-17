259d1b9944c57ac009702366c9286ba98013add8
endioliveira <eomedeiros21@gmail.com>
2023-01-17 15:33:22 -0300

    retornando os usuários e produtos, mockando um termo de busca, inserindo um item mockado na tabela de users e de products, buscar por um produto, deletar um usuario e produt, editar eles, ordenar uma coluna e limitar dados

--- src/database/labecommerce.sql
+++ src/database/labecommerce.sql
@@ -32,3 +32,45 @@ VALUES ("p01", "Body Splash", 35.00, "Cosméticos"),
        ("p03", "Pulseira", 17.50, "Acessórios"),
        ("p04", "Delineador", 30.75, "Maquiagens"),
        ("p05", "Batom Matte", 15.20, "Maquiagens");
+
+SELECT * FROM users;
+
+SELECT * FROM products;
+
+SELECT * FROM products
+WHERE name = "Body Splash";
+
+INSERT INTO users (id, email, password)
+VALUES ("4", "adriana@gmail.com", "ad12345");
+
+INSERT INTO products (id, name, price, category)
+VALUES ("p06", "Perfume", 100.00, "Cosméticos");
+
+SELECT * FROM products
+WHERE id = "p04";
+
+DELETE FROM users
+WHERE id = "2";
+
+DELETE FROM products
+WHERE id = "p03";
+
+UPDATE users
+SET email = "mariajulia@gmail.com"
+WHERE id = "1";
+
+UPDATE products
+SET name = "Gloss Pink"
+WHERE id = "p05";
+
+SELECT * FROM users
+ORDER BY email ASC;
+
+SELECT * FROM products
+ORDER BY price ASC
+LIMIT 20
+OFFSET 0;
+
+SELECT * FROM products
+WHERE price >= 20 AND price <= 35 
+ORDER BY price ASC;

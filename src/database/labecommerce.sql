086a11eece8eaa47cb479c0844e3ee4e52efcc93
Endi Oliveira <100172961+endioliveira@users.noreply.github.com>
2023-01-18 19:11:12 -0300

    :sparkles: feat: criando a tabela de pedidos

--- src/database/labecommerce.sql
+++ src/database/labecommerce.sql
@@ -74,3 +74,33 @@ OFFSET 0;
 SELECT * FROM products
 WHERE price >= 20 AND price <= 35 
 ORDER BY price ASC;
+
+-- EXERCÍCIOS DE RELAÇÕES EM SQL I - DATA 18-01-23
+
+CREATE TABLE purchases (
+    id TEXT PRIMARY KEY UNIQUE NOT NULL,
+    total_price REAL UNIQUE NOT NULL,
+    paid INTEGER NOT NULL,
+    delivered_at TEXT,
+    buyer_id TEXT NOT NULL,
+    FOREIGN KEY (buyer_id) REFERENCES users(id)
+);
+
+INSERT INTO purchases (id, total_price, paid, delivered_at, buyer_id)
+VALUES ("pu01", 70.00, 1, NULL,"1"),
+       ("pu02", 45.00, 0,NULL, "1"),
+       ("pu03", 17.50, 1, NULL, "2"),
+       ("pu04", 92.25, 0, NULL, "3");
+
+DROP TABLE purchases;
+
+SELECT * FROM purchases;
+
+UPDATE purchases
+SET delivered_at = DATETIME('now')
+WHERE id = "pu04";
+
+SELECT * FROM purchases
+INNER JOIN users
+ON purchases.buyer_id = users.id
+WHERE users.id = "1";
